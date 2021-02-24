c6ed2a72152028edbce638d9642fd21e13295615
ashwin_kumar_mobiotics <ashwin.kumar@mobiotics.com>
2021-02-24 23:49:25 +0530

    manifest correction.

--- scripts/load.js
+++ scripts/load.js
@@ -1,8 +1,15 @@
-import {el} from './supports.js';
+import {el,createEl} from './supports.js';
 let postLoad=(self)=>{
+    
+    //add manifest
+    
+    let manifest=createEl('link',{rel:'manifest',href:`${self.location.origin}/manifest.json`});
+    el('head').append(manifest);   
+    
+    //manifest added.
+
      self.addEventListener('load',()=>{
         el('.header-text').classList.add('show-header');
     })
 }
-
 export default postLoad;
--- scripts/supports.js
+++ scripts/supports.js
@@ -1,1 +1,11 @@
+//fetch an element
 export let el=(selector,parent=document)=>parent.querySelector(selector);
+
+//create an element
+export let createEl=(tagname,attributes)=>{
+                        let el=document.createElement(tagname);
+                        Object.entries(attributes).forEach(([name,value]) => {
+                                el.setAttribute(name,value);
+                        });   
+                        return el;
+                    }
