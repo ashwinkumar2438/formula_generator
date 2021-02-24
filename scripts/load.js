5b7291992e724d24fdd2887341181798ef387311
ashwin_kumar_mobiotics <ashwin.kumar@mobiotics.com>
2021-02-25 00:19:34 +0530

    manifest fix.

--- scripts/load.js
+++ scripts/load.js
@@ -1,12 +1,5 @@
 import {el,createEl} from './supports.js';
 let postLoad=(self)=>{
-    
-    //add manifest
-    
-    let manifest=createEl('link',{rel:'manifest',href:`${self.location.origin}/manifest.json`});
-    el('head').append(manifest);   
-    
-    //manifest added.
 
      self.addEventListener('load',()=>{
         el('.header-text').classList.add('show-header');
