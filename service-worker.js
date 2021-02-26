bfcf72ba2abd5febbe81341683dd4e2e5294d301
ashwin_kumar_mobiotics <ashwin.kumar@mobiotics.com>
2021-02-27 02:25:18 +0530

    update cache

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ let passUrl=(url='')=>{
     else return passChecks.some(check=>url.includes(check));
 }
 
-let cacheName='static-files-02';
+let cacheName='static-files-03';
 
 self.addEventListener('activate',(e)=>{
     e.waitUntil(caches.keys().then(keys=>Promise.all(
